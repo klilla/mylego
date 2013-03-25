57c74de3e928c5bdd8ca5a0751e14e687c0b6028
Katarzyna Lilla <katarzyna.lilla@gmail.com>
2013-03-25 22:03:40 +0100

    Missing changes.

--- mylego/settings.py
+++ mylego/settings.py
@@ -115,6 +115,7 @@ ROOT_URLCONF = 'mylego.urls'
 WSGI_APPLICATION = 'mylego.wsgi.application'
 
 TEMPLATE_DIRS = (
+    _rel_path('templates'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
